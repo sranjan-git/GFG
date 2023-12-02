5c4c646b5ddee0684e554c12547b1682bdb9321d
Sudhanshu Ranjan <86006320+sranjan-git@users.noreply.github.com>
2023-12-02 23:55:02 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Inorder Traversal and BST/inorder-traversal-and-bst.cpp
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    int isRepresentingBST(int arr[], int N)
+    {
+        // code here
+    int prev = INT_MIN;
+
+    for (int i = 0; i < N; ++i) {
+        if (arr[i] <= prev) {
+            return false;
+        }
+        prev = arr[i];
+    }
+
+    return true;
+}
+
+int main() {
+    int N;
+    cout << "Enter the size of the array: ";
+    cin >> N;
+
+    vector<int> arr(N);
+    cout << "Enter the elements of the array: ";
+    for (int i = 0; i < N; ++i) {
+        cin >> arr[i];
+    }
+
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin >> N;
+        int arr[N];
+        for(int i=0;i<N;i++)cin>>arr[i];
+        Solution ob;
+        cout<<ob.isRepresentingBST(arr,N)<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
