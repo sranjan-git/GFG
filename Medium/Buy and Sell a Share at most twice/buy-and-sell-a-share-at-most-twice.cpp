22d3db1df85b5d6b46836ba48f5a9c5814a9a5d1
Sudhanshu Ranjan <86006320+sranjan-git@users.noreply.github.com>
2024-02-23 22:52:30 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Buy and Sell a Share at most twice/buy-and-sell-a-share-at-most-twice.cpp
@@ -0,0 +1,86 @@
+//{ Driver Code Starts
+//Initial Template for C++
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution
+{
+    public:
+        //User function Template for C++
+        
+        int maxProfit(vector<int>&price){
+            //Write your code here..
+            int n = price.size();
+            if(n==1)
+            {
+                return 0;
+            }
+            else if(n==2)
+            {
+                int ans = price[1]-price[0];
+                if(ans<=0)
+                {
+                    return 0;
+                }
+                else
+                {
+                    return ans;
+                }
+            }
+            vector<int>leri(n);
+            vector<int>rile(n);
+            int mini = price[0];
+            leri[0] = 0;
+            for(int i = 1; i < n; i++)
+            {
+                mini = min(mini,price[i]);
+                leri[i] = max(leri[i-1],price[i]-mini);
+            }
+            int maxi = price[n-1];
+            rile[n-1] = 0;
+            for(int i = n-2; i>=0; i--)
+            {
+                maxi = max(maxi,price[i]);
+                rile[i] = max(rile[i+1],maxi-price[i]);
+            }
+            int ans = 0;
+            for(int i = 0; i < n; i++)
+            {
+                if(i==0)
+                {
+                    ans = max(ans,leri[i]+rile[i+1]);
+                }
+                else if(i==n-1)
+                {
+                    ans = max(ans,leri[i]);
+                }
+                else
+                {
+                    ans = max(ans,leri[i]+rile[i+1]);
+                }
+            }
+            return ans;
+        }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        
+        int n;
+        cin>>n;
+        vector<int> price(n);
+        for(int i=0;i<n;i++) cin>>price[i];
+        Solution obj;
+        cout<<obj.maxProfit(price)<<endl;
+    }
+
+}
+
+// } Driver Code Ends
