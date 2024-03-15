aaa9bd0fd06149b09920c4deb4821c7b62c86a1a
Sudhanshu Ranjan <86006320+sranjan-git@users.noreply.github.com>
2024-03-15 22:29:29 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Linked List that is Sorted Alternatingly/linked-list-that-is-sorted-alternatingly.cpp
@@ -0,0 +1,110 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+	int data;
+	struct Node *next;
+	
+	Node(int x){
+	    data =x;
+	    next = NULL;
+	}
+};
+/* Function to print linked list */
+
+
+void append(struct Node** headRef, struct Node** tailRef, int newData)
+{
+	struct Node* new_node = new Node(newData);
+	struct Node *last = *headRef;
+	if (*headRef == NULL)
+	{
+	    *headRef = new_node;
+	    *tailRef = *headRef;
+	    return;
+	}
+	
+	(*tailRef) -> next = new_node;
+	*tailRef = (*tailRef) -> next;
+}
+
+// A utility function to print a linked list
+void printList(Node *head)
+{
+	while (head != NULL)
+	{
+		cout << head->data << " ";
+		head = head->next;
+	}
+	cout << endl;
+}
+
+
+
+// } Driver Code Ends
+
+/*
+struct Node
+{
+	int data;
+	struct Node *next;
+	
+	Node(int x){
+	    data =x;
+	    next = NULL;
+	}
+};
+*/
+
+class Solution
+{
+    public:
+    // your task is to complete this function
+     void sort(Node **head)
+    {
+         // Code here
+         if((*head)==NULL || (*head)->next==NULL) return ;
+         Node *temp=*head;
+         vector<int>v;
+         while(temp){
+             auto val=temp->data;
+             v.push_back(val);
+             temp=temp->next;
+         }
+         
+        std:: sort(v.begin(),v.end());
+         
+         temp=*head;
+         
+        for(int i=0;i<v.size();i++){
+            temp->data=v[i];
+            temp=temp->next;
+        }
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+	int test;
+	cin>>test;
+	while(test--)
+	{
+	    struct Node* head = NULL;
+	    struct Node* tail = NULL;
+	    int n, k;
+	    cin>>n;
+	    for(int i=0; i<n ;i++){
+	        cin>>k;
+	        append(&head, &tail, k);
+	    }
+	    Solution ob;
+	    ob.sort(&head);
+	    printList(head);
+	}
+	return 0;
+}
+
+// } Driver Code Ends
