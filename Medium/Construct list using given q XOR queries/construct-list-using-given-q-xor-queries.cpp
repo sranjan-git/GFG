12d026d0123237082e26b923c3e5186d478d52d4
Sudhanshu Ranjan <86006320+sranjan-git@users.noreply.github.com>
2024-06-02 23:09:16 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Construct list using given q XOR queries/construct-list-using-given-q-xor-queries.cpp
@@ -0,0 +1,96 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+class Matrix {
+  public:
+    template <class T>
+    static void input(vector<vector<T>> &A, int n, int m) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                scanf("%d ", &A[i][j]);
+            }
+        }
+    }
+
+    template <class T>
+    static void print(vector<vector<T>> &A) {
+        for (int i = 0; i < A.size(); i++) {
+            for (int j = 0; j < A[i].size(); j++) {
+                cout << A[i][j] << " ";
+            }
+            cout << endl;
+        }
+    }
+};
+
+class Array {
+  public:
+    template <class T>
+    static void input(vector<T> &A, int n) {
+        for (int i = 0; i < n; i++) {
+            scanf("%d ", &A[i]);
+        }
+    }
+
+    template <class T>
+    static void print(vector<T> &A) {
+        for (int i = 0; i < A.size(); i++) {
+            cout << A[i] << " ";
+        }
+        cout << endl;
+    }
+};
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+      vector<int> constructList(int q, vector<vector<int>> &queries) 
+    {
+   vector<int> result = {0};  
+    int cumulativeXOR = 0;  
+    for (int i = 0; i < q; ++i) 
+    {
+        int type = queries[i][0];
+        int value = queries[i][1];
+        if (type == 0) 
+        {
+            result.push_back(value ^ cumulativeXOR); 
+        }
+        else if (type == 1) 
+        {
+            cumulativeXOR ^= value;
+        }
+    }
+    for (int j = 0; j < result.size(); ++j) 
+    {
+        result[j] ^= cumulativeXOR;
+    }
+     sort(result.begin(), result.end());
+        return result;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    scanf("%d ", &t);
+    while (t--) {
+
+        int q;
+        scanf("%d", &q);
+
+        vector<vector<int>> queries(q, vector<int>(2));
+        Matrix::input(queries, q, 2);
+
+        Solution obj;
+        vector<int> res = obj.constructList(q, queries);
+
+        Array::print(res);
+    }
+}
+
+// } Driver Code Ends
