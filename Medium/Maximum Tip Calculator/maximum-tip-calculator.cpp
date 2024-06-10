b4966ef3162b5651ea1c83bfed43578d53f160d4
Sudhanshu Ranjan <86006320+sranjan-git@users.noreply.github.com>
2024-06-11 02:51:05 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Maximum Tip Calculator/maximum-tip-calculator.cpp
@@ -0,0 +1,96 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+class Array {
+  public:
+    template <class T>
+    static void input(vector<T> &a, int n) {
+        for (int i = 0; i < n; i++) {
+            scanf("%d ", &a[i]);
+        }
+    }
+
+    template <class T>
+    static void print(vector<T> &a) {
+        for (int i = 0; i < a.size(); i++) {
+            cout << a[i] << " ";
+        }
+        cout << endl;
+    }
+};
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    long long maxTip(int n, int x, int y, vector<int> &arr, vector<int> &brr) {
+        // code here
+        long long maxt=0;
+        for(int i=0;i<n;i++){
+            brr[i]=brr[i]-arr[i];
+            maxt+=arr[i];
+        }
+        sort(brr.begin(),brr.end());
+        int i=0;
+        while(brr[i]<=0 && i<x){
+            i++;
+        }
+        if(i==x){
+            while(i<n){
+                maxt+=brr[i];
+                i++;
+            }
+            return maxt;
+        }
+        else{
+            if(n-i<=y){
+                while(i<n){
+                    maxt+=brr[i];
+                    i++;
+                }
+                return maxt;
+            }
+            else{
+                i=n-y;
+                while(i<n){
+                    maxt+=brr[i];
+                    i++;
+                }
+                return maxt;
+            }
+        }
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    scanf("%d ", &t);
+    while (t--) {
+
+        int n;
+        scanf("%d", &n);
+
+        int x;
+        scanf("%d", &x);
+
+        int y;
+        scanf("%d", &y);
+
+        vector<int> arr(n);
+        Array::input(arr, n);
+
+        vector<int> brr(n);
+        Array::input(brr, n);
+
+        Solution obj;
+        long long res = obj.maxTip(n, x, y, arr, brr);
+
+        cout << res << endl;
+    }
+}
+
+// } Driver Code Ends
