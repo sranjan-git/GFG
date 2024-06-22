5990d7e077992fb75cd45e25b39b36101a5e27b0
Sudhanshu Ranjan <86006320+sranjan-git@users.noreply.github.com>
2024-06-22 23:56:35 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Extract the Number from the String/extract-the-number-from-the-string.cpp
@@ -0,0 +1,73 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+private:
+    bool isNumber(const string& word) {
+        for (char c : word) {
+            if (!isdigit(c)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    bool containsNine(const string& number) {
+        for (char c : number) {
+            if (c == '9') {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+public:
+    long long ExtractNumber(string sentence) {
+        stringstream ss(sentence);
+        string word;
+        long long largestNumber = -1;
+
+        while (ss >> word) {
+            if (isNumber(word) && !containsNine(word)) {
+                long long num = stoll(word);
+                if (num > largestNumber) {
+                    largestNumber = num;
+                }
+            }
+        }
+
+        return largestNumber;
+    }
+
+};
+
+//{ Driver Code Starts.
+int main() {
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+    string tc;
+    getline(cin, tc);
+    t = stoi(tc);
+    while (t--) {
+        string s;
+        getline(cin, s);
+
+        Solution ob;
+        cout << ob.ExtractNumber(s) << "\n";
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
