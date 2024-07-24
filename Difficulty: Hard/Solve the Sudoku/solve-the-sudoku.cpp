cc8b26de2311647288ee4c8f8d7792ba5beac33e
Sudhanshu Ranjan <86006320+sranjan-git@users.noreply.github.com>
2024-07-24 21:46:09 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Solve the Sudoku/solve-the-sudoku.cpp
@@ -0,0 +1,94 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+// UNASSIGNED is used for empty cells in sudoku grid 
+#define UNASSIGNED 0  
+
+// N is used for the size of Sudoku grid.  
+// Size will be NxN  
+#define N 9  
+
+
+// } Driver Code Ends
+class Solution 
+{
+    public:
+    //Function to find a solved Sudoku. 
+    bool isValid(int row,int col,int grid[N][N],int p){
+        for(int i=0;i<9;i++){
+            if(grid[row][i]==p){
+                return false;
+            }
+        }
+        for(int i=0;i<9;i++){
+            if(grid[i][col]==p){
+                return false;
+            }
+        }
+        for(int i=0;i<9;i++){
+            if(grid[3*(row/3)+i/3][3*(col/3)+i%3]==p){
+                return false;
+            }
+        }
+        return true;
+    }
+    bool SolveSudoku(int grid[N][N])  
+    { 
+        // Your code here
+         for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                if(grid[i][j]==0){
+                    for(int p=1;p<=9;p++){
+                      if(isValid(i,j,grid,p)){
+                          grid[i][j]=p;
+                          if(SolveSudoku(grid))
+                          {return true;
+                          }
+                          grid[i][j]=0;
+                }
+            }return false;
+         }
+    }
+         }return true;
+    }
+    
+    //Function to print grids of the Sudoku.
+    void printGrid (int grid[N][N]) 
+    {
+        // Your code here 
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                cout<<grid[i][j]<<" ";
+            }
+        }
+        
+       
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int grid[N][N];
+		
+		for(int i=0;i<9;i++)
+		    for(int j=0;j<9;j++)
+		        cin>>grid[i][j];
+		        
+		Solution ob;
+		
+		if (ob.SolveSudoku(grid) == true)  
+            ob.printGrid(grid);  
+        else
+            cout << "No solution exists";  
+        
+        cout<<endl;
+	}
+	
+	return 0;
+}
+// } Driver Code Ends
