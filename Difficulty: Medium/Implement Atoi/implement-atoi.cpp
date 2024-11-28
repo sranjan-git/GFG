f14b0fa9bd84d86a5b67f1d6b43539111ff53046
Sudhanshu Ranjan <86006320+sranjan-git@users.noreply.github.com>
2024-11-28 23:52:47 +0530

    Updated Solution - GfG to GitHub

---
+++ Difficulty: Medium/Implement Atoi/implement-atoi.cpp
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+
+class Solution {
+public:
+    int myAtoi(char *s) {
+        int i = 0, sign = 1;
+        long result = 0;
+
+        // Skip leading whitespaces
+        while (s[i] == ' ') {
+            i++;
+        }
+
+        // Check the sign
+        if (s[i] == '-') {
+            sign = -1;
+            i++;
+        } else if (s[i] == '+') {
+            i++;
+        }
+
+        // Convert digits to integer
+        while (s[i] >= '0' && s[i] <= '9') {
+            result = result * 10 + (s[i] - '0');
+            i++;
+
+            // Handle overflow cases
+            if (result * sign > INT_MAX) {
+                return INT_MAX;
+            } else if (result * sign < INT_MIN) {
+                return INT_MIN;
+            }
+        }
+
+        return (int)(result * sign);
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        char s[20];
+        cin >> s;
+        Solution ob;
+        int ans = ob.myAtoi(s);
+        cout << ans << endl;
+        cout << "~" << endl;
+    }
+}
+// } Driver Code Ends
